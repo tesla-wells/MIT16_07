b9a9e51b29a67d95f862ea6837c0ba76415647cf
Tesla Wells <teslaw@mac-56-129-07.mit.edu>
2019-10-16 16:26:42 -0400

    Progress on two, figures separate

--- PSET6/springSystemDynamics.m
+++ PSET6/springSystemDynamics.m
@@ -8,6 +8,7 @@ L0 = sqrt(2);
 
 energy = @(x) (k.*(sqrt(h_0.^2 + x.^2) - L0).^2 ./2);
 
+figure('Name', 'Energy vs x of cart')
 hold on
 fplot(energy, [-2 2])
 xlabel('X location')
@@ -15,6 +16,12 @@ ylabel('Energy stored in spring')
 title ('Energy vs x of cart')
 hold off
 
-horizForce = @(x) (k.*(sqrt(h_0.^2 + x.^2) - L0));
+horizForce = @(x) (x.*(k.*(sqrt(h_0.^2 + x.^2) - L0)./(sqrt(h_0.^2 + x.^2))));
 
+figure('Name', 'Effective Force')
+hold on
 fplot(horizForce, [-2 2])
+xlabel('X location')
+ylabel('Horizontal Force')
+title ('Effective Force')
+hold off
